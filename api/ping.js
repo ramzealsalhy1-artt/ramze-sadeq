7f3dcb760b7edf6375322c7034dc276cc363657f
رمزي الصلاحي <ramzealsalhy1@gmail.com>
2026-03-28 04:32:35 +0300

    Add ping handler with Redis environment checks

--- api/ping.js
+++ api/ping.js
@@ -1,1 +1,9 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    message: 'pong',
+    env: {
+      hasRedisUrl: !!process.env.UPSTASH_REDIS_REST_URL,
+      hasRedisToken: !!process.env.UPSTASH_REDIS_REST_TOKEN,
+    }
+  });
+}
